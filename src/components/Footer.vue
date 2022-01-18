d9870fcaa436623f1c05d5b19c0fd364e05eb0e3
橙梓 <me@orangii.cn>
2022-01-18 23:53:25 +0800

    添加萌ICP号

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,12 +3,14 @@
     <a href="https://orangii.cn" target="_blank" rel="noopener" style="color: #e4a240;">&copy; Orangii</a>
     <a href="https://myssl.com/huli.li" target="_blank" rel="noopener noreferrer" style="color: #66c18c"><i class="fas fa-lock">&NonBreakingSpace;Safe</i></a>
     <a href="https://orangii.cn/friends/" target="_blank" style="color: #e4a240;"><i class="fas fa-link"></i>&nbsp;友情连接</a>
+    <a href="https://icp.gov.moe/?keyword=20225511" target="_blank" rel="noopener" style="color: #ee4866;"><i class="moe-icp"></i>&nbsp;萌ICP备20225511号</a>
     <a href="https://github.com/Jiaocz/huli.li" target="_blank" rel="noopener noreferrer"><img src="https://img.shields.io/github/stars/Jiaocz/huli.li?style=social" alt="Github Star"></a>
   </footer>
 </template>
 
 <script lang="ts" setup>
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import '../assets/moe-icp/index.css';
 </script>
 
 <style scoped>
