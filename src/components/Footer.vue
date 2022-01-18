14d8416471f3dd24c7f9428981ab86cb22c462a1
橙梓 <me@orangii.cn>
2022-01-18 23:37:25 +0800

    添加Github图标

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,6 +3,7 @@
     <a href="https://orangii.cn" target="_blank" rel="noopener" style="color: #e4a240;">&copy; Orangii</a>
     <a href="https://myssl.com/huli.li" target="_blank" rel="noopener noreferrer" style="color: #66c18c"><i class="fas fa-lock">&NonBreakingSpace;Safe</i></a>
     <a href="https://orangii.cn/friends/" target="_blank" style="color: #e4a240;"><i class="fas fa-link"></i>&nbsp;友情连接</a>
+    <a href="https://github.com/Jiaocz/huli.li" target="_blank" rel="noopener noreferrer"><img src="https://img.shields.io/github/stars/Jiaocz/huli.li?style=social" alt="Github Star"></a>
   </footer>
 </template>
 
@@ -17,6 +18,11 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 
   .footer a {
     text-decoration: none;
+  }
+
+  .footer img {
+    height: 1em;
+    transform: translateY(2px);
   }
 
   .footer > *::after {
