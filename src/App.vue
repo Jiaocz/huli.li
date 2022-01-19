e5038a5d55eb70bc5d57e5bf23092fb8b8c41f0e
橙梓 <me@orangii.cn>
2022-01-19 17:51:30 +0800

    定时任务和修订版本号

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, onMounted, onBeforeUnmount } from 'vue';
 import { IPosition } from './data/interfaces/position';
 import { ISun } from './data/interfaces/sun';
 import { getLocation } from './services/location'
@@ -11,21 +11,56 @@ import Clock from './components/Clock.vue';
 import Footer from './components/Footer.vue';
 
 let sunStatus = ref<ISun>();
-let position = ref<IPosition>();
 let isDaytime = ref(true);
 let weather = ref<IWeather>();
-(async () => {
+let position: IPosition;
+let weatherTimerId: number;
+let sunTimerId: number;
+let today: number;
+
+const init = async () => {
+  await initLocation();
+  today = new Date().getDay();
+  await getWeather();
+  weatherTimerId = setInterval(getWeather, 60 * 60 * 1000);
+  await getSunStatus();
+  await checkStatus();
+  sunTimerId = setInterval(checkStatus, 45 * 1000);
+}
+
+const initLocation = async () => {
   const locate = await getLocation();
-  position.value = locate.result.location;
-  const sun = await getSunRiseAndSet(locate.result.location);
-  sunStatus.value = sun;
+  position = locate.result.location;
+}
 
-  const now = new Date();
-  isDaytime.value = new Date(sunStatus.value.sunrise).getTime() < now.getTime() && new Date(sunStatus.value.sunset).getTime() > now.getTime();
-
-  const weatherNow = await realtimeWeather(position.value);
+const getWeather = async () => {
+  const weatherNow = await realtimeWeather(position);
   weather.value = weatherNow;
-})();
+}
+
+const getSunStatus = async () => {
+  const sun = await getSunRiseAndSet(position);
+  sunStatus.value = sun;
+}
+
+const checkStatus = async () => {
+  if (sunStatus.value) {
+    const now = new Date();
+    if (now.getDay() != today) {
+      await getSunStatus();
+    }
+    isDaytime.value = new Date(sunStatus.value.sunrise).getTime() < now.getTime() && new Date(sunStatus.value.sunset).getTime() > now.getTime();
+  } else {
+    getSunStatus();
+    checkStatus();
+  }
+};
+
+onMounted(init);
+onBeforeUnmount(() => {
+  clearInterval(weatherTimerId);
+  clearInterval(sunTimerId);
+});
 </script>
 
 <template>
