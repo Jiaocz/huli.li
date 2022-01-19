8c8604a2033d54c33c1f2d5d88a1a84ad25f3d55
橙梓 <me@orangii.cn>
2022-01-19 13:48:29 +0800

    修改Readme内容和API Key解释

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
-# huli.li 狐狸狸
+# [huli.li](https://huli.li/) 狐狸狸
 域名一直没有建站，想尝试使用Vue-ts进行一次试手
 
 使用[Vite.js](https://vitejs.dev)建立项目，请求使用的是jQuery，天气接口使用的和风天气，位置接口使用腾讯地图。
 
 ## 如何使用
+
+### 更改API Key
+更改`src/data/common/keys.ts`中的API_KEY，具体申请请进入文件查看申请链接
 ### 安装依赖
 ```shell
 cnpm install
--- src/data/common/keys.ts
+++ src/data/common/keys.ts
@@ -1,2 +1,11 @@
+/**
+ * 腾讯地图API KEY，**限定使用域名**使用时需要修改
+ * 申请地址：https://lbs.qq.com/dev/console/application/mine
+ */
 export const TMAP_KEY = 'T2DBZ-JTO3F-2G7JB-NXUR7-SNY3O-7TBAM';
+
+/**
+ * 和风天气API，需要自行申请
+ * 申请地址：https://id.qweather.com/#/login
+ */
 export const QWEATHER_KEY = 'ed95368f5ed4427f9ad8594d5b1a6850';
