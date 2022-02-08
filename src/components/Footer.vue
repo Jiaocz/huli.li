0bbebcdb83fb93f3d49ef6f73f78a3edee1836c5
Orangii <14857984+Jiaocz@users.noreply.github.com>
2022-02-08 22:13:13 +0800

    修改文字位置和空格

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,9 @@
 <template>
   <footer class="footer">
     <a href="https://orangii.cn" target="_blank" rel="noopener" style="color: #e4a240;">&copy; Orangii</a>
-    <a href="https://myssl.com/huli.li" target="_blank" rel="noopener noreferrer" style="color: #66c18c"><i class="fas fa-lock">&NonBreakingSpace;Safe</i></a>
-    <a href="https://orangii.cn/friends/" target="_blank" style="color: #e4a240;"><i class="fas fa-link"></i>&nbsp;友情连接</a>
-    <a href="https://icp.gov.moe/?keyword=20225511" target="_blank" rel="noopener" style="color: #ee4866;"><i class="moe-icp"></i>&nbsp;萌ICP备20225511号</a>
+    <a href="https://myssl.com/huli.li" target="_blank" rel="noopener noreferrer" style="color: #66c18c"><i class="fas fa-lock"></i>&NonBreakingSpace;Safe</a>
+    <a href="https://orangii.cn/friends/" target="_blank" style="color: #e4a240;"><i class="fas fa-link"></i>&NonBreakingSpace;友情连接</a>
+    <a href="https://icp.gov.moe/?keyword=20225511" target="_blank" rel="noopener" style="color: #ee4866;"><i class="moe-icp"></i>&NonBreakingSpace;萌ICP备20225511号</a>
     <a href="https://github.com/Jiaocz/huli.li" target="_blank" rel="noopener noreferrer"><img src="https://img.shields.io/github/stars/Jiaocz/huli.li?style=social" alt="Github Star"></a>
   </footer>
 </template>
